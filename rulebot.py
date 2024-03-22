4e72c46b0468c6fcf6d2528acc5c649672fe522d
cmd <161607150+cmd-codee@users.noreply.github.com>
2024-03-23 01:11:08 +0300

    Update rulebot.py

--- rulebot.py
+++ rulebot.py
@@ -9,20 +9,20 @@ def start(message):
     bot.send_photo(message.chat.id, 'https://s.poembook.ru/theme/97/c2/0f/d7bcb057973c48bdd7e272c063f37355114770e0.jpeg', caption=f'Привет {message.from_user.first_name}!\nЯ бот для игры в русскую рулетку в группе или канале. Добавляй меня и играй. Удачи в игре!')
 @bot.message_handler(commands=['newgame'])
 def game(message):
-    markup2 = types.ReplyKeyboardMarkup(row_width=1)
-    item2 = types.KeyboardButton('1 патрон')
-    item3 = types.KeyboardButton('Все патроны кроме одного')
-    markup2.add(item2, item3)
-    bot.send_message(message.chat.id, 'Выберите режим игры', reply_markup=markup2)
+    markup = types.ReplyKeyboardMarkup(row_width=1)
+    item = types.KeyboardButton('1 патрон')
+    item2 = types.KeyboardButton('Все патроны кроме одного')
+    markup.add(item, item2)
+    bot.send_message(message.chat.id, 'Выберите режим игры', reply_markup=markup)
 @bot.message_handler(content_types=['text'])
 def text(message):
     if message.text == 'Все патроны кроме одного':
         players.clear()
-        markup3 = types.ReplyKeyboardMarkup(row_width=1)
-        item4 = types.KeyboardButton('Присоединиться')
-        item5 = types.KeyboardButton('Играть')
-        markup3.add(item4, item5)
-        bot.send_message(message.chat.id, 'Нажмите присоединится (играют минимум 2 человека), после того как все присоединяться нажмите Играть!', reply_markup=markup3)
+        markup2 = types.ReplyKeyboardMarkup(row_width=1)
+        item3 = types.KeyboardButton('Присоединиться')
+        item4 = types.KeyboardButton('Играть')
+        markup2.add(item3, item4)
+        bot.send_message(message.chat.id, 'Нажмите присоединится (играют минимум 2 человека), после того как все присоединяться нажмите Играть!', reply_markup=markup2)
     if message.text == 'Присоединиться':
         msf = message.from_user.first_name
         if msf not in players:
@@ -38,11 +38,11 @@ def text(message):
             	     	bot.send_photo(message.chat.id, 'https://i.ytimg.com/vi/_oaSxfBUyhc/maxresdefault.jpg', caption=f'{players} остался(-ась) в живых! Игра окончена!')
     if message.text == '1 патрон':
         players2.clear()
-        markup4 = types.ReplyKeyboardMarkup(row_width=1)
-        item6 = types.KeyboardButton('Присоединиться к игре')
-        item7 = types.KeyboardButton('Начать игру')
-        markup4.add(item6, item7)
-        bot.send_message(message.chat.id, 'Нажмите Присоединиться к игре (играют минимум 2 человека), после того как все присоединяться нажмите Начать игру!', reply_markup=markup4)
+        markup3 = types.ReplyKeyboardMarkup(row_width=1)
+        item5 = types.KeyboardButton('Присоединиться к игре')
+        item6 = types.KeyboardButton('Начать игру')
+        markup3.add(item5, item6)
+        bot.send_message(message.chat.id, 'Нажмите Присоединиться к игре (играют минимум 2 человека), после того как все присоединяться нажмите Начать игру!', reply_markup=markup3)
     if message.text == 'Присоединиться к игре':
         msf2 = message.from_user.first_name
         if msf2 not in players2:
@@ -59,4 +59,3 @@ while True:
         bot.polling(none_stop=True)
     except Exception as _ex:
         print(_ex)
-        sleep(15)
